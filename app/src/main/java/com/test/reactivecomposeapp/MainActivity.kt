3e1c39680210598e395e284e4fc01f11bcdd7088
DJDrama <putzzang@naver.com>
2023-04-27 01:14:10 +0900

    First Commit

---
+++ app/src/main/java/com/test/reactivecomposeapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.test.reactivecomposeapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.test.reactivecomposeapp.ui.theme.ReactiveComposeAppTheme
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val windowSize = calculateWindowSizeClass(this)
+            ReactiveComposeAppTheme {
+                Log.d("MainActivity", "WindowSize : " + windowSize)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ReactiveComposeAppTheme {
+        Greeting("Android")
+    }
+}
